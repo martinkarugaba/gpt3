3244aab6dd84132c0e6a691dffb08e8ad329d21b
Martin Karugaba <martinkarugaba21@gmail.com>
2022-03-29 12:40:13 +0300

    added general styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div className='gpt__navbar'>
+    <div className="gpt__navbar-links">
+      <div className="gpt3__navbar-links_logo">
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  </div>;
+};
 
-export default Navbar
+export default Navbar;
