05f396ad34d1c19a781aa8479ee8dea8f02002cd
Martin Karugaba <martinkarugaba21@gmail.com>
2022-05-12 23:23:59 +0300

    started header section

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.scss";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header section__padding" id="home">
+      <div className="header__content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing.
+          Not thoughts all exercise blessing. Indulgence way
+          everything joy alteration boisterous the attachment. Party
+          we years to order allow asked of.
+        </p>
+
+        <div className="header__contentInput">
+          <input
+            type="email"
+            placeholder="Your Email Address"
+            name=""
+            id=""
+          />
+          <button type="button">Get Started</button>
+        </div>
+
+        <div className="header__contentPeople">
+          <img src={people} alt="people" />
+          <p>
+            1,600 people requested access a visit in last 24 hours
+          </p>
+        </div>
+
+        <div className="header__image">
+          <img src={ai} alt={ai} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
