1a2d0daaa63414fb457d7a473aa4ea64b5f70b7a
Martin Karugaba <martinkarugaba21@gmail.com>
2022-05-17 23:58:52 +0300

    added possibility and cta sections

--- src/components/cta/Cta.js
+++ src/components/cta/Cta.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import './cta.css';
+import React from "react";
+import "./cta.scss";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="cta section__margin">
+      <div className="cta__content">
+        <p>Request Early Access to Get Started</p>
+        <h2>
+          Register today & start exploring the endless possiblities.
+        </h2>
+      </div>
+      <div className="cta__button">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
