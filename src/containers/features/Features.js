11adbd6ee10ec25baca1773f14931f203c15521e
Martin Karugaba <martinkarugaba21@gmail.com>
2022-05-20 22:15:09 +0300

    fixed desktop typography

--- src/containers/features/Features.js
+++ src/containers/features/Features.js
@@ -8,14 +8,18 @@ const Features = () => {
     <div className="features__section section__padding" id="features">
       <div className="features__heading">
         <h2 className="gradient__text">
-          The future is now and you just need to realize It. Step into the
+          The Future is Now and You Just Need To Realize It. Step into
           Future Today & Make it Happen.
         </h2>
         <p>Request Early Access to Get Started</p>
       </div>
       <section className="features__container">
         {featuresData.map((item, index) => (
-          <Feature title={item.title} text={item.text} key={item.title + index} />
+          <Feature
+            title={item.title}
+            text={item.text}
+            key={item.title + index}
+          />
         ))}
       </section>
     </div>
